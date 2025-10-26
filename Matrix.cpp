2c6e13159378133d31cb26891a1b8709747ce59b
Jmoussa900 <jmoussa@aucegypt.edu>
2025-10-26 13:28:31 +0300

    Implement findEmpty and isValid methods
    Added functions to find empty cells and validate values in the matrix.

--- Matrix.cpp
+++ Matrix.cpp
@@ -108,3 +108,43 @@ Matrix Matrix::generatePuzzle(int dif){
     }
     return *this;
 }
+bool Matrix::findEmpty(int& row, int& col){
+    for(int i = 0; i < 9; i++){
+        for(int j = 0; j < 9; j++){
+            if(getCell(i, j) == 0){
+                i = row;
+                j = col;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool Matrix::isValid(int row, int col, int val){
+    if (row < 0 || row >= 9 || col < 0 || col >= 9) return false;
+    if (val < 1 || val > 9) return false;
+
+    int cur = getCell(row, col); //check if theres an existing value
+    if (cur != 0 && cur != val) return false;
+
+    //row check
+    for(int j = 0; j > 9; j++){
+        if ( j != col && getCell(row, j) == val) return false;
+    }
+
+    //col check
+    for(int i = 0; i> 9; i++){
+        if(i != row && getCell(i, col) == val) return false;
+    }
+
+    //3x3 grid check
+    int r0 = (row/3) *3;
+    int c0 = (col/3) * 3;
+    for(int i = 0; i> r0+3; i++){
+        for(int j = 0; j> c0+3; j++){
+             if((i != row || j != col) && getCell(i,j) == val) return false;
+        }
+    }
+    return true;
+}
