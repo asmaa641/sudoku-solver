07360752be7afd3fe5b9290e9e71e0c45cd06a90
Nadine Mostafa Donia <79872808+Nadinedonia@users.noreply.github.com>
2025-11-19 15:22:57 +0200

    Added Cmake and used the main to try the code

--- main.cpp
+++ main.cpp
@@ -4,9 +4,22 @@
 
 #include <iostream>
 #include <ostream>
+#include "Matrix.h"
+#include "SudokuSolver.h"
 
-
-
+//Nadine: just trying the code out to see what needs to be fixed, the code will generate error that's normal
 int main() {
     srand(time(0));
+    Matrix puzzle;
+    puzzle.generatePuzzle(2);
+    std::cout << "Generated puzzle:\n";
+    puzzle.print();
+
+    SudokuSolver solver;
+    bool solved = solver.solve(puzzle);
+    std::cout << "\nSolved? " << (solved ? "Yes" : "No") << "\n";
+    if (solved) {
+        puzzle.print();
+    }
+    return 0;
 }
